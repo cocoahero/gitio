08bf38f4fd47c79051ea6e483cec63bc251f7c08
Jonathan Baker <jonathanmbaker@me.com>
2011-11-11 16:29:25 -0500

    Moved all logic into exe

--- lib/gitio.rb
+++ lib/gitio.rb
@@ -1,23 +1,4 @@
-require "net/http"
 require "gitio/version"
 
 module GitIO
-  SERVICE_URL = "http://git.io/"
-  
-  def self.shrink(url, options = {})
-    raise "You must specifiy a URL to shrink!" if url.nil?
-    
-    post_params = { url: url }
-    post_params[:code] = options[:name] unless options[:name].nil?
-    
-    response = Net::HTTP.post_form(URI.parse(SERVICE_URL), post_params)
-    
-    case response
-    when Net::HTTPCreated then
-      return response['location']
-    else
-      return nil
-    end
-  end
-  
 end
